115177034d12f3ab74c6b8f07ab48f0d86995e3e
Bertto <nachebertah@gmail.com>
2022-08-05 10:49:22 -0300

    deleted deprecated code

--- components/Containers/FooterInsideTop/FooterInsideTop.tsx
+++ components/Containers/FooterInsideTop/FooterInsideTop.tsx
@@ -2,28 +2,12 @@ import styles from './FooterInsideTop.styles';
 import Text36P from '../../Texts/Center/36P_White_Bold/Text36P_W';
 import Text24P_L from '../../Texts/Left/24P_W/L_Text24P_W';
 
-
-import Text24P_W from '../../Texts/Center/24P_White/Text24P_W';
-import Text16P_W from '../../Texts/Center/16P_White/Text16P_W';
-import {useState, useEffect} from 'react';
-
 function FooterInsideTop(props: {
     title: string,
     content1: string,
     content2?: string,
     content3?: string,
 }){
-
-    const [windowWidth, setWindowWidth] = useState(0);
-
-    useEffect(() => {
-        setWindowWidth(window.innerWidth);
-        if(windowWidth){
-            window.addEventListener('resize', () => {
-                setWindowWidth(window.innerWidth);
-            });
-        }
-    }, [windowWidth]);
 
     return (
         <div
